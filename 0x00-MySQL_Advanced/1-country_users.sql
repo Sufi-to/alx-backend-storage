f680de81a38a46b1bbef185bc90b1d267a8e9a8f
Sufi-to <3thomas225@gmail.com>
2024-08-07 22:20:54 +0400

    advanced sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create a users table with constraints
+CREATE TABLE IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+        PRIMARY KEY (id)
+);
