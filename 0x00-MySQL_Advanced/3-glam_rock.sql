307f98aa6f7a0b3b22d083404ec225df28f9851d
Sufi-to <3thomas225@gmail.com>
2024-08-09 01:14:33 +0400

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE style LIKE '%Glam rock%' AND formed <= 2022
 ORDER BY lifespan DESC;
