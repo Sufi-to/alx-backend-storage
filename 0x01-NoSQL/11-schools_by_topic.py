3ace9f4a36230245153546c6fec438aacd4fbb2c
Sufi-to <3thomas225@gmail.com>
2024-08-14 00:49:37 +0400

    filter db

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that shows how to interact to a mongo db"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic"""
+    topic_filter = {'topics': {'$elemMatch': {'$eq': topic}}}
+    return [doc for doc in mongo_collection.find(topic_filter)]
