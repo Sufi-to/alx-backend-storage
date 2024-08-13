37999a67a75fec2ed4ccf20d4ced28ba5caabc93
Sufi-to <3thomas225@gmail.com>
2024-08-14 02:53:23 +0400

    stats logged

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,22 +10,23 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     len_coll = nginx_collection.count_documents({})
-    print(len_coll, 'logs')
+    print("{} logs".format(len_coll))
     print('Methods:')
-    print('\tmethod GET: ', nginx_collection.count_documents(
+    print("\tmethod GET: ".format(nginx_collection.count_documents(
         {'$expr': {'$eq': ['$method', 'GET']}})
-        )
-    print('\tmethod POST: ', nginx_collection.count_documents(
+        ))
+    print("\tmethod POST: ".format(nginx_collection.count_documents(
         {'$expr': {'$eq': ['$method', 'POST']}})
-        )
-    print('\tmethod PUT: ', nginx_collection.count_documents(
+        ))
+    print("\tmethod PUT: ".format(nginx_collection.count_documents(
         {'$expr': {'$eq': ['$method', 'PUT']}})
-          )
-    print('\tmethod Patch: ', nginx_collection.count_documents(
+        ))
+    print("\tmethod Patch: ".format(nginx_collection.count_documents(
         {'$expr': {'$eq': ['$method', 'PATCH']}})
-        )
-    print('\tmethod delete: ', nginx_collection.count_documents(
+        ))
+    print("\tmethod delete: {}".format(nginx_collection.count_documents(
         {'$expr': {'$eq': ['$method', 'DELETE']}})
+        ))
+    print("{} status check".format(
+        len(list(nginx_collection.find({'method': 'GET', 'path': '/status'}))))
         )
-    print(len(list(nginx_collection.
-          find({'method': 'GET', 'path': '/status'}))), 'status_check')
