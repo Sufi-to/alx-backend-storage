e7ac7ba60187afb4cd8867bd676d06a1a1dc6448
Sufi-to <3thomas225@gmail.com>
2024-08-17 02:21:56 +0400

    counts the method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,19 @@
 
 import redis
 import uuid
+from functools import wraps
 from typing import Any, Union, Optional, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """Keeps count of many times the method is called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper function of the store method"""
+        method_name = method.__qualname__
+        self._redis.incr(method_name)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -14,6 +26,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key and store the data using that key"""
         id = str(uuid.uuid4())
